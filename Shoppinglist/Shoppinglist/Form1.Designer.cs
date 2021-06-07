f72355ba9ded86eec8dbe74b57092b4df8f7866a
ShivKaur <76859654+ShivKaur@users.noreply.github.com>
2021-06-07 14:14:27 +1000

    Add files via upload

---
+++ Shoppinglist/Shoppinglist/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace Shoppinglist
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.combo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.txt_qty = new System.Windows.Forms.TextBox();
+            this.txt_total = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.addbutton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_sub = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(135, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(451, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // combo
+            // 
+            this.combo.FormattingEnabled = true;
+            this.combo.Location = new System.Drawing.Point(236, 218);
+            this.combo.Name = "combo";
+            this.combo.Size = new System.Drawing.Size(318, 23);
+            this.combo.TabIndex = 2;
+            this.combo.SelectedIndexChanged += new System.EventHandler(this.combo_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Item";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(152, 177);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 19);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Milk";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(409, 177);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(51, 19);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Juice";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(236, 177);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 19);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Eggs";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(317, 177);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(55, 19);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Bread";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // txt_price
+            // 
+            this.txt_price.Location = new System.Drawing.Point(135, 284);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.Size = new System.Drawing.Size(100, 23);
+            this.txt_price.TabIndex = 8;
+            this.txt_price.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_qty
+            // 
+            this.txt_qty.Location = new System.Drawing.Point(263, 284);
+            this.txt_qty.Name = "txt_qty";
+            this.txt_qty.PlaceholderText = "Enter quantity ";
+            this.txt_qty.Size = new System.Drawing.Size(100, 23);
+            this.txt_qty.TabIndex = 9;
+            this.txt_qty.TextChanged += new System.EventHandler(this.txt_qty_TextChanged);
+            // 
+            // txt_total
+            // 
+            this.txt_total.Location = new System.Drawing.Point(384, 284);
+            this.txt_total.Name = "txt_total";
+            this.txt_total.Size = new System.Drawing.Size(100, 23);
+            this.txt_total.TabIndex = 22;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Price";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Quantity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(399, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Total Price";
+            // 
+            // addbutton
+            // 
+            this.addbutton.Location = new System.Drawing.Point(528, 284);
+            this.addbutton.Name = "addbutton";
+            this.addbutton.Size = new System.Drawing.Size(75, 23);
+            this.addbutton.TabIndex = 14;
+            this.addbutton.Text = "Add Item";
+            this.addbutton.UseVisualStyleBackColor = true;
+            this.addbutton.Click += new System.EventHandler(this.addbutton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(135, 336);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(492, 97);
+            this.listView1.TabIndex = 15;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Item Name";
+            this.columnHeader1.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Price";
+            this.columnHeader2.Width = 70;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Qty";
+            this.columnHeader3.Width = 70;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Total";
+            this.columnHeader4.Width = 70;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(135, 453);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Sub total";
+            // 
+            // txt_sub
+            // 
+            this.txt_sub.Location = new System.Drawing.Point(195, 450);
+            this.txt_sub.Name = "txt_sub";
+            this.txt_sub.Size = new System.Drawing.Size(56, 23);
+            this.txt_sub.TabIndex = 19;
+            this.txt_sub.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(317, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(116, 292);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 15);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 537);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_sub);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.addbutton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_total);
+            this.Controls.Add(this.txt_qty);
+            this.Controls.Add(this.txt_price);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combo);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox combo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox txt_price;
+        private System.Windows.Forms.TextBox txt_qty;
+        private System.Windows.Forms.TextBox txt_total;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button addbutton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_sub;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Shoppinglist/Shoppinglist/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace Shoppinglist
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            radioButton1.ForeColor = System.Drawing.Color.Green;
+            radioButton2.ForeColor = System.Drawing.Color.Red;
+            radioButton3.ForeColor = System.Drawing.Color.Red;
+            radioButton4.ForeColor = System.Drawing.Color.Red;
+
+            combo.Items.Clear();
+            combo.Items.Add("Full Cream Milk");
+            combo.Items.Add("Lite Reduced Fat Milk");
+            combo.Items.Add("Skim Milk");
+
+
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.ForeColor = System.Drawing.Color.Red;
+            radioButton2.ForeColor = System.Drawing.Color.Red;
+            radioButton3.ForeColor = System.Drawing.Color.Green;
+            radioButton4.ForeColor = System.Drawing.Color.Red;
+
+            combo.Items.Clear();
+            combo.Items.Add("Free Range Eggs");
+            combo.Items.Add("Cage Free Extra Large Eggs");
+            combo.Items.Add("Farm Fresh Cage Eggs");
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.ForeColor = System.Drawing.Color.Red;
+            radioButton2.ForeColor = System.Drawing.Color.Red;
+            radioButton3.ForeColor = System.Drawing.Color.Red;
+            radioButton4.ForeColor = System.Drawing.Color.Green;
+
+            combo.Items.Clear();
+            combo.Items.Add("Multi-Grain Bread");
+            combo.Items.Add("White Bread");
+            combo.Items.Add("Wholemeal Sandwich Bread");
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.ForeColor = System.Drawing.Color.Red;
+            radioButton2.ForeColor = System.Drawing.Color.Green;
+            radioButton3.ForeColor = System.Drawing.Color.Red;
+            radioButton4.ForeColor = System.Drawing.Color.Red;
+
+            combo.Items.Clear();
+            combo.Items.Add("Orange Juice");
+            combo.Items.Add("Apple Juice");
+            combo.Items.Add("Mango Juice");
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (combo.SelectedItem.ToString() == "Full Cream Milk")
+            {
+                txt_price.Text = "3.35";
+            }
+            else if (combo.SelectedItem.ToString() == "Lite Reduced Fat Milk")
+            {
+                txt_price.Text = "3.35";
+            }
+            else if (combo.SelectedItem.ToString() == "Skim Milk")
+            {
+                txt_price.Text = "3.35";
+            }
+            else if (combo.SelectedItem.ToString() == "Free Range Eggs")
+            {
+                txt_price.Text = "3.50";
+            }
+            else if (combo.SelectedItem.ToString() == "Cage Free Extra Large Eggs")
+            {
+                txt_price.Text = "4.00";
+            }
+            else if (combo.SelectedItem.ToString() == "Farm Fresh Cage Eggs")
+            {
+                txt_price.Text = "5.50";
+            }
+            else if (combo.SelectedItem.ToString() == "Multi-Grain Bread")
+            {
+                txt_price.Text = "3.30";
+            }
+            else if (combo.SelectedItem.ToString() == "White Bread")
+            {
+                txt_price.Text = "3.90";
+            }
+            else if (combo.SelectedItem.ToString() == "Wholemeal Sandwich Bread")
+            {
+                txt_price.Text = "4.45";
+            }
+            else if (combo.SelectedItem.ToString() == "Orange Juice")
+            {
+                txt_price.Text = "2.50";
+            }
+            else if (combo.SelectedItem.ToString() == "Apple Juice")
+            {
+                txt_price.Text = "3.00";
+            }
+            else if (combo.SelectedItem.ToString() == "Mango Juice")
+            {
+                txt_price.Text = "3.90";
+            }
+            else
+            {
+                txt_price.Text = "0";
+            }
+            txt_total.Text = "";
+            txt_qty.Text = "";
+        }
+        private void txt_qty_TextChanged(object sender, EventArgs e)
+        {
+            
+                if (txt_qty.Text.Length > 0)
+                {
+                    txt_total.Text = (Convert.ToDecimal(txt_price.Text) * Convert.ToDecimal(txt_qty.Text)).ToString();
+                }
+            
+           
+
+        }
+
+        private void addbutton_Click(object sender, EventArgs e)
+        {
+            string[] arr = new string[4];
+            arr[0] = combo.SelectedItem.ToString();
+            arr[1] = txt_price.Text;
+            arr[2] = txt_qty.Text;
+            arr[3] = txt_total.Text;
+            ListViewItem lvi = new ListViewItem(arr);
+            listView1.Items.Add(lvi);
+            txt_sub.Text = (Convert.ToDecimal(txt_sub.Text) + Convert.ToDecimal(txt_total.Text)).ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+           if (listView1.Items.Count>0)
+            {
+                try 
+                {
+                     string Con = "Data Source=DESKTOP-PFO1JF7\\SQLEXPRESS;Initial Catalog=Shoppinglist;Integrated Security=True";
+                    SqlConnection connection = new SqlConnection(Con);
+                    SqlCommand command = connection.CreateCommand();
+
+                    connection.Open();
+                    command.CommandText = "Insert into Invoice_table(Invoice_Date, Sub_total) values" +
+                        "(getdate(), " + txt_sub.Text + ") select scope_identity()";
+
+                    string Invoice_ID = command.ExecuteScalar().ToString();
+                    int Invoice = Int16.Parse(Invoice_ID);
+                    //for(int j=0; j<listView1.Items.Count; j++)
+                    //{
+                    //  int k=0;
+                    //MessageBox.Show(listView1.Items[j].SubItems[k].Text );
+                    //MessageBox.Show(listView1.Items[j].SubItems[k+1].Text );
+                    //MessageBox.Show (listView1.Items[j].SubItems[k+2].Text );
+                    //MessageBox.Show (listView1.Items[j].SubItems[k+3].Text );
+
+                    //}
+                   // SqlCommand cmd;
+                   // cmd= new SqlCommand("insert into Detail_table(Master_ID, Item_name, Item_price, Item_qty, Item_total) values(@Master_ID, @Item_name, @Item_price, @Item_qty, @Item_total)", connection);
+                    //cmd.Parameters.Add("@Master_ID", (SqlDbType)Invoice);
+                    
+                        for (int j = 0; j < listView1.Items.Count; j++)
+                        {
+                            int k = 0;
+
+                       // MessageBox.Show(Int16.Parse(listView1.Items[j].SubItems[k].Text));
+                        // MessageBox.Show(Invoice_ID);
+                        // MessageBox.Show(listView1.Items[j].SubItems[k+1].Text);
+                        //MessageBox.Show(listView1.Items[j].SubItems[k+2].Text);
+                        //MessageBox.Show(listView1.Items[j].SubItems[k+3].Text);
+                        //MessageBox.//Show(listView1.Items[i].SubItems[ii].Text);
+                        // command.CommandText = "Insert into detaaa(Master_ID) values" + "("+ Invoice +") select scope_identity()";
+                        //command.ExecuteNonQuery();
+                        //MessageBox.Show("Successful");
+                        command.CommandText = "Insert into Detail_table(Master_ID,Item_name, Item_price, Item_qty, Item_total) values" +
+                    "('" + Invoice + "','" + listView1.Items[j].SubItems[k].Text + "'," + listView1.Items[j].SubItems[k+1].Text + ",'" + listView1.Items[j].SubItems[k+2].Text + "," + listView1.Items[j].SubItems[k+3].Text + ") select scope_identity()";
+                        //
+                        //command.ExecuteNonQuery();
+                        //command.Parameters["Master_ID"].Value = Invoice;
+                       // command.Parameters["Item_name"].Value = listView1.Items[j].SubItems[k].Text;
+                    }
+                    // ListItem.SubItems[3].Text
+                    //int r;
+                    //r= command.ExecuteNonQuery();
+                      //  if(r>=1)
+                    //{
+                      //  MessageBox.Show("Records inserted");
+                    //}
+                    //}
+                    //command.ExecuteNonQuery();
+                    connection.Close();
+                    MessageBox.Show("Sale Created Successfully, with Invoice # " + Invoice);
+                }
+                catch(Exception ee)
+                {
+                    
+                    MessageBox.Show("Error!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Must add an item in the list");
+            }
+        }
+    }
+    
+}
